e5c27838898a557eee8e403d92885508fdbb461e
Zhenhailong Wang <wangz3@blender07.cs.illinois.edu>
2022-06-17 18:58:43 -0500

    update gpt3 results

--- scripts/train_caption_video.sh
+++ scripts/train_caption_video.sh
@@ -1,7 +1,7 @@
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 echo "number of GPU: $N_GPU"
 
-CONFIG_NAME=$1 # e.g., train_blip_video_captioning_msrvtt_pseudo
+CONFIG_NAME=$1 # e.g., train_blip_video_captioning_msrvtt
 
 python -m torch.distributed.run --nproc_per_node=$N_GPU train_caption_video.py \
 --config configs/${CONFIG_NAME}.yaml \
